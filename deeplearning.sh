364b09391e077d64469d197fa013076e36928dcd
Animesh Bala Ani <62231754+ANI717@users.noreply.github.com>
2021-01-21 16:08:53 -0500

    Update deeplearning.sh

--- deeplearning.sh
+++ deeplearning.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt update
-sudo apt install libopenblas-dev libblas-dev m4 cmake cython python3-yaml libatlas-base-dev
+sudo apt install libopenblas-dev libblas-dev m4 cmake cython python3-yaml libatlas-base-dev -y
 export ONNX_ML=1
 cd ~ && git clone --recursive https://github.com/pytorch/pytorch
 cd pytorch
